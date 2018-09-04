3eb190f72413ddd05716321e36d02d6d7ca22439
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-04 14:42:14 -0400

    add blogSchema

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ var express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/Blog_RESTfulRoutes");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+});
+//title
+//image
+//body
+//created
+
+app.listen(8887, () => {
+  console.log("The blog server has started!");
+});
